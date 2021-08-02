88cdf79adafb581dfb5b29183474b22e236587d4
vavesparashar <vavesparashar@gmail.com>
2021-08-02 18:38:23 +0530

    helmet

--- src/components/footer.js
+++ src/components/footer.js
@@ -15,7 +15,9 @@ const Footer = () => {
 
           <div className="row"> 
              <p>
+               <Helmet>
               <script async src="https://eomail5.com/form/f674b022-f25f-11eb-96e5-06b4694bee2a.js" data-form="f674b022-f25f-11eb-96e5-06b4694bee2a"></script>
+              </Helmet>
              </p>
           </div>
 
