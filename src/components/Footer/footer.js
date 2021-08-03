bd8b1414ab0120a263409b15d073a58b17d195fd
vavesparashar <vavesparashar@gmail.com>
2021-08-04 03:48:08 +0530

    Updating footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import { Link } from 'gatsby';
+import Link from 'next/link';
 
-import SocialIcons from './social-icons';
+import useSite from 'hooks/use-site';
 
-// import image_resume from '../../static/assets/colby-fayock-front-end-development-web-design-resume.pdf';
+import SocialIcons from 'components/SocialIcons';
+
+import Section from 'components/Section';
+import Container from 'components/Container';
+import FormSignupNewsletter from 'components/FormSignupNewsletter';
+
+import styles from './Footer.module.scss';
 
 const Footer = () => {
+  const { metadata = {} } = useSite();
+  const { title } = metadata;
+
   return (
-    <div className="footer">
+    <div className={styles.footer}>
+      
       <Section>
         <Container className={styles.footerEmailSignup}>
           <p>Get free content and updates straight to your inbox!</p>
@@ -16,44 +25,44 @@ const Footer = () => {
         </Container>
       </Section>
 
-            <footer className="container footer-primary" role="contentinfo">
+      <footer>
+        <Section>
+          <Container className={styles.footerPrimary} role="contentinfo">
+            <SocialIcons className={styles.footerSocial} />
 
-        <div className="content">
-
-          <div className="row">
-
-            <div className="footer-social">
-              <SocialIcons />
-            </div>
-
-            <div className="twelvecol footer-links align-center">
-
-              <ul className="flat-top">
-                <li>
-                  <Link to="/about">
-                    About
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/all-posts">
-                    All Posts
-                  </Link>
-                </li>
-                
-                <li>
-                  <Link to="/">
-                    &copy;Pixels
-                  </Link>
-                </li>
-              </ul>
-
-            </div>
-
-          </div>
-
-        </div>
-
+            <ul className={styles.footerLinks}>
+              <li>
+                <Link href="/about">
+                  <a>About</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/posts">
+                  <a>All Posts</a>
+                </Link>
+              </li>
+              <li>
+                <a href="/assets/colby-fayock-front-end-development-web-design-resume.pdf">Resume</a>
+              </li>
+              <li>
+                <Link href="/">
+                  <a>&copy; {title}</a>
+                </Link>
+              </li>
+            </ul>
+          </Container>
+        </Section>
       </footer>
+
+      <div hidden className="h-card sr-only">
+        <a className="p-name u-url" href="https://marketingbaba.in" rel="me">
+          Vivek Vardhan
+        </a>
+        <img className="u-photo" src="https://www.colbyfayock.com/colby-fayock-bite-world.jpg" alt="Vivek Vardhan" />
+        <p className="p-note">
+          A digital marketer's journal
+        </p>
+      </div>
     </div>
   );
 };
