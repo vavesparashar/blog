71c3c0e56b56b5a01eb2a641e8c6bd63bff5bca7
vavesparashar <vavesparashar@gmail.com>
2021-08-03 10:16:43 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -20,11 +20,7 @@ const Footer = () => {
           <Helmet>
         <script src={withPrefix('https://eomail5.com/form/f674b022-f25f-11eb-96e5-06b4694bee2a.js')} type="text/javascript" />
     </Helmet>
-             
-             <p>
-              <script async src="https://eomail5.com/form/f674b022-f25f-11eb-96e5-06b4694bee2a.js" data-form="f674b022-f25f-11eb-96e5-06b4694bee2a"></script>
-             </p>
-          </div>
+               </div>
 
         </div>
 
